ecff729c041d3ed75371981b49c368fe4257fd8b
wahyukahrisma <madewahyu39@gmail.com>
2021-03-30 15:04:25 +0700

    Initialize assets and library

---
+++ app/src/main/java/com/example/movist/presentation/view/splashScreen/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.example.movist.presentation.view.splashScreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.movist.databinding.ActivitySplashScreenBinding
+import com.example.movist.presentation.view.dashboard.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var _binding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = _binding.root
+        setContentView(view)
+
+
+        val r = Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed(r, 3000)
+
+    }
+}
