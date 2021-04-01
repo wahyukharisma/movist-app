7a41bc0f3f0cc18277bc369735a37f2bd110854c
wahyukahrisma <madewahyu39@gmail.com>
2021-04-01 16:33:55 +0700

    ! Remove chuck

--- app/src/main/java/com/example/movist/di/NetworkModule.kt
+++ app/src/main/java/com/example/movist/di/NetworkModule.kt
@@ -1,15 +1,12 @@
 package com.example.movist.di
 
 import com.example.movist.BuildConfig
-import com.example.movist.presentation.BaseApplication
 import com.example.movist.services.network.MovieServices
 import com.example.movist.util.Constants
-import com.readystatesoftware.chuck.ChuckInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Named
@@ -19,18 +16,12 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private val okHttpClient =
-        OkHttpClient.Builder()
-            .addNetworkInterceptor(ChuckInterceptor(BaseApplication.appContext))
-            .build()
-
     @Singleton
     @Provides
     fun provideService() : MovieServices{
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(okHttpClient)
             .build()
             .create(MovieServices::class.java)
     }
--- app/src/main/java/com/example/movist/presentation/BaseApplication.kt
+++ app/src/main/java/com/example/movist/presentation/BaseApplication.kt
@@ -9,14 +9,9 @@ import timber.log.Timber
 @HiltAndroidApp
 class BaseApplication : Application() {
 
-    companion object {
-        lateinit var appContext: Context
-    }
-
     override fun onCreate() {
         super.onCreate()
 
-        appContext = applicationContext
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         Timber.plant(Timber.DebugTree())
     }
